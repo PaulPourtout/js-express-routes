a8bb408d435db8edf3d2777456d2fad5587abfb5
PaulPourtout <paulpourtout@hotmail.com>
2017-04-03 18:39:35 +0200

    add firebase

---
+++ index.js
@@ -0,0 +1,54 @@
+// Configure express
+var express = require('express');
+var app = express();
+
+// Import EJS
+var ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+// Import data users
+var users = require('./model/data.js');
+var firebase = require('firebase');
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyA88-qaOkFbx0_1vLM6q3uLPJvYuFHdicE",
+  authDomain: "users-list-77a0a.firebaseapp.com",
+  databaseURL: "https://users-list-77a0a.firebaseio.com",
+  storageBucket: "users-list-77a0a.appspot.com"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+function writeUserData(userId, username, description) {
+  db.ref('users/' + userId).set({
+	  userId: userId,
+    username: username,
+	description: description
+  });
+}
+// writeUserData(0 , 'michou', 'michou est le plus beau');
+
+db.ref('users').on('value', (snapshot) => console.log(snapshot.val()));
+
+// Static route
+app.use(express.static(__dirname + '/public'));
+// Defining routes
+// app.get('/', function(req, res) {
+// 	res.send('Ceci est une Home Page (même si elle n\'en a pas l\'air).');
+// })
+
+app.get('/', function(req, res) {
+	// console.log(firebase.auth().user);
+  res.render('./pages/index.ejs', {users: users});
+}).get('/user/:id', function(req, res) {
+  res.render('./pages/user.ejs', {
+    user: users[req.params.id]
+  });
+}).get('/*', function(req, res) {
+  res.status(404).render('./pages/error.ejs');
+}).listen(5000, function(req, res) {
+  console.log('The server is OK. Now you can connect to localhost:5000.');
+});
---
+++ model/data.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    id: 0,
+    firstName: 'Michel',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }, {
+    id: 1,
+    firstName: 'Osman',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }, {
+    id: 2,
+    firstName: 'Tandi',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }, {
+    id: 3,
+    firstName: 'Daniel',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }, {
+    id: 4,
+    firstName: 'Faustino',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }, {
+    id: 5,
+    firstName: 'Ijacques',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
+
+module.exports = users;
